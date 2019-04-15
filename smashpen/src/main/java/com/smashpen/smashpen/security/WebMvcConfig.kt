021733a652eb58111d05f67ab2cfea0f7bf6c64b
DeanIsseyegh <issdea@gmail.com>
2019-04-15 21:48:31 +0100

    Fixed new component bean wiring with uservalidation interceptor

--- smashpen/src/main/java/com/smashpen/smashpen/security/WebMvcConfig.kt
+++ smashpen/src/main/java/com/smashpen/smashpen/security/WebMvcConfig.kt
@@ -1,22 +1,15 @@
 package com.smashpen.smashpen.security
 
-import com.smashpen.smashpen.service.AccessValidationService
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebMvcConfig(private val accessValidationService: AccessValidationService) : WebMvcConfigurer {
+class WebMvcConfig(private val userValidationInterceptor: UserValidationInterceptor) : WebMvcConfigurer {
 
     override fun addInterceptors(registry: InterceptorRegistry) {
-        registry.addInterceptor(userValidationInterceptor()).addPathPatterns("/**/character")
+        registry.addInterceptor(userValidationInterceptor).addPathPatterns("/**/character")
         super.addInterceptors(registry)
-    }
-
-    @Bean
-    fun userValidationInterceptor() : UserValidationInterceptor {
-        return UserValidationInterceptor(accessValidationService)
     }
 
 }
